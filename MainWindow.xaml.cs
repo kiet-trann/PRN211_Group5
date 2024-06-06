a011013eb62f44062348e3208106542b9b55c2c5
kiet-trann <kiettuantranle@gmail.com>
2024-06-06 09:04:19 +0700

    bai day lay di

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManageCategoriesApp1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnInsert_Click(object sender, RoutedEventArgs e)
+		{
+			var category = new Category { CategoryName = txtCategoryName.Text };
+			ManageCategories.Instance.InsertCategory(category);
+			LoadCategories();
+		}
+
+		private void btnUpdate_Click(object sender, RoutedEventArgs e)
+		{
+			var category = new Category
+			{
+				CategoryID = int.Parse(txtCategoryID.Text),
+				CategoryName = txtCategoryName.Text
+			};
+			ManageCategories.Instance.UpdateCategory(category); LoadCategories();
+			
+		
+		}
+
+		private void btnDelete_Click(object sender, RoutedEventArgs e)
+		{
+			var category = new Category { CategoryID = int.Parse(txtCategoryID.Text) };
+			ManageCategories.Instance.DeleteCategory(category);
+			LoadCategories();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			LoadCategories();
+		}
+		public void LoadCategories()
+		{
+			var categories = ManageCategories.Instance.GetCategories();
+			lvCategories.ItemsSource = categories;
+			
+		}
+	}
+}
---
+++ ManageCategories.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageCategoriesApp1
+{
+	public sealed class ManageCategories
+	{
+		//Using Singleton Pattern
+		private static ManageCategories instance = null;
+		private static readonly object instanceLock = new object();
+		private ManageCategories() { }
+		public static ManageCategories Instance
+		{
+			get
+			{
+				lock (instanceLock)
+				{
+					if (instance == null)
+					{
+						instance = new ManageCategories();
+					}
+					return instance;
+				}
+			}
+		}
+		public List<Category> GetCategories()
+		{
+			List<Category> categories;
+			try
+			{
+				using MyStock myStock = new MyStock();
+				categories = myStock.Categories.ToList();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+			return categories;
+		}// end GetCategories
+		 //----------------------------------------------------------------------------------
+		public void InsertCategory(Category category)
+		{
+			try
+			{
+				using MyStock stock = new MyStock();
+				stock.Categories.Add(category);
+				stock.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}//end InsertCategory
+		 //------------------------------------------------------------------------------------
+		public void DeleteCategory(Category category)
+		{
+			try
+			{
+				using MyStock stock = new MyStock();
+				//Find Category by CategoryID
+				var cate = stock.Categories.SingleOrDefault(c => c.CategoryID == category.CategoryID);
+				stock.Categories.Remove(cate);
+				stock.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}// end DeleteCAtegory
+		//------------------------------------------------------------------------------------------------
+		public void UpdateCategory(Category category)
+		{
+			try
+			{
+				using MyStock stock = new MyStock();
+				stock.Categories.Update(category);
+				stock.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}//end UpdateCategory
+	}
+}
